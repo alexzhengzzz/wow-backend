2573186c6528a3a26090caa359ff02728c90b92f
Zhe Zhang <zz3230@nyu.edu>
2022-05-03 22:51:43 -0400

    规范化Response回传包

--- src/main/java/com/controller/CarInfoController.java
+++ src/main/java/com/controller/CarInfoController.java
@@ -1,11 +1,12 @@
 package com.controller;
 
+import com.bo.CarInfoBO;
 import com.business.CarInfoBusiness;
+import com.enums.ResponseCode;
 import com.utils.cache.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,19 +20,19 @@ public class CarInfoController {
 
     @ApiOperation("get entire carList")
     @GetMapping("entire")
-    public Response getAllCarList(){
-        return new Response<>(carInfoBusiness.getCarList(0));
+    public Response<List<CarInfoBO>> getAllCarList(){
+        return new Response<>(ResponseCode.SUCCESS,carInfoBusiness.getCarList(0));
     }
 
     @ApiOperation("get in-stock carList")
     @GetMapping("valid")
-    public Response getValidCarList(){
-        return new Response<>(carInfoBusiness.getCarList(1));
+    public Response<List<CarInfoBO>> getValidCarList(){
+        return new Response<>(ResponseCode.SUCCESS, carInfoBusiness.getCarList(1));
     }
 
     @ApiOperation("get invalid carlist")
     @GetMapping("invalid")
-    public Response getInvalidCarList(){
-        return new Response<>(carInfoBusiness.getCarList(2));
+    public Response<List<CarInfoBO>> getInvalidCarList(){
+        return new Response<>(ResponseCode.SUCCESS, carInfoBusiness.getCarList(2));
     }
 }
