121c6d997bb09fe99b2f42d0299d0519922eea24
alexzhengzzz <839910307@qq.com>
2022-05-08 15:51:36 -0400

    bug fix: getOrderByUserId 找不到车不返回的问题

--- src/main/java/com/business/impl/RentalOrderBusinessImpl.java
+++ src/main/java/com/business/impl/RentalOrderBusinessImpl.java
@@ -19,6 +19,7 @@ import com.vo.orders.OrderInvoiceVO;
 import com.vo.orders.OrderListVO;
 import com.vo.orders.OrderVO;
 import com.vo.orders.OrderVehicleVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +31,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@Slf4j
 public class RentalOrderBusinessImpl implements RentalOrderBusiness {
     @Autowired
     private VehicleServiceImpl vehicleService;
@@ -272,9 +274,15 @@ public class RentalOrderBusinessImpl implements RentalOrderBusiness {
         orderVO.setOrderStatus(rentalOrder.getOrderStatus());
         orderVO.setInvoiceId(rentalOrder.getInvoiceId());
         orderListVO.setOrderVO(orderVO);
-        vehicleService.getOne(new LambdaQueryWrapper<Vehicle>().eq(Vehicle::getVinId, rentalOrder.getVinId()));
         OrderVehicleVO orderVehicleVO = new OrderVehicleVO();
-        Vehicle vehicle = vehicleService.getVehicleById(rentalOrder.getVinId());
+        orderListVO.setInvoiceId(rentalOrder.getInvoiceId());
+        Vehicle vehicle = null;
+        try {
+            vehicle = vehicleService.getVehicleById(rentalOrder.getVinId());
+        } catch (Exception e) {
+            log.warn("car not found" + e.getMessage() + rentalOrder.getVinId());
+            return orderListVO;
+        }
         Integer classId = vehicle.getClassId();
         CarClass carClass = carClassService.getCarClassInfoById(classId);
         Model model = modelService.getById(vehicle.getModelId());
@@ -282,8 +290,6 @@ public class RentalOrderBusinessImpl implements RentalOrderBusiness {
         orderVehicleVO.setVinId(vehicle.getVinId());
         orderVehicleVO.setImgUrl(carClass.getImageUrl());
         orderListVO.setOrderVehicleVO(orderVehicleVO);
-
-        orderListVO.setInvoiceId(rentalOrder.getInvoiceId());
         return orderListVO;
     }
 }
