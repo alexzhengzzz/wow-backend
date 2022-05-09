f0eb24806586cb911854c2dfad1aedd70a85964d
alexzhengzzz <839910307@qq.com>
2022-05-09 17:44:24 -0400

    fix TimestampUtil

--- src/main/java/com/utils/cache/TimestampUtil.java
+++ src/main/java/com/utils/cache/TimestampUtil.java
@@ -2,12 +2,28 @@ package com.utils.cache;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
 
+import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.Date;
 
+@Slf4j
 public class TimestampUtil {
     public static Long getDiffDays(Timestamp timestamp1, Timestamp timestamp2) {
-        return DateUtil.between(new Date(timestamp1.getTime()), new Date(timestamp2.getTime()), DateUnit.DAY);
+        Date date1 = new Date(timestamp1.getTime());
+        Date date2 = new Date(timestamp2.getTime());
+        Long days = DateUtil.between(date1, date2, DateUnit.DAY); // 1
+        Long hours = DateUtil.between(date1, date2, DateUnit.HOUR); // 25
+        Long minutes = DateUtil.between(date1, date2, DateUnit.MINUTE); // 1439
+        Long seconds = DateUtil.between(date1, date2, DateUnit.SECOND); // 86400
+
+        log.warn("days: {}", days);
+        log.warn("hours: {}", hours);
+        log.warn("minutes: {}", minutes);
+        log.warn("seconds: {}", seconds);
+        if (hours > 0 || minutes > 0 || seconds > 0) {
+            return days + 1;
+        }
+        return days;
     }
 }
